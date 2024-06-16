a01843eb235a879bb48cc8934ed6414481069e73
RinkaMizuki <nguyenduc09012003@gmail.com>
2024-06-17 00:30:49 +0700

    change at cookie not httponly

--- src/controllers/v1/authController.js
+++ src/controllers/v1/authController.js
@@ -1,230 +1,260 @@
-import { changePassword, confirmEmail, enableF2A, forgotPassword, googleLink, loginFacebook, loginGoogle, postLogin, postLogout, refreshToken, registerUser, resetPassword, unlinkGoogle, verifyOtp } from "../../services/authService";
-import { verifyFacebookJWT, verifyGoogleJWT, verifyJWT } from "../../services/jwtService";
+import {
+    changePassword,
+    confirmEmail,
+    enableF2A,
+    forgotPassword,
+    googleLink,
+    loginFacebook,
+    loginGoogle,
+    postLogin,
+    postLogout,
+    refreshToken,
+    registerUser,
+    resetPassword,
+    unlinkGoogle,
+    verifyOtp,
+} from "../../services/authService";
+import {
+    verifyFacebookJWT,
+    verifyGoogleJWT,
+    verifyJWT,
+} from "../../services/jwtService";
 import { cookieExpires } from "../../services/timeExpires";
 
 function setCookie(res, data, cookieATage, cookieRTage) {
-  res.cookie("refreshToken", data.refreshToken,
-    {
-      maxAge: cookieRTage,
-      httpOnly: true,
-    }
-  )
-  if (data.user?.role !== "admin") {
-    res.cookie("accessToken", data.accessToken,
-      {
-        maxAge: cookieATage,
+    res.cookie("refreshToken", data.refreshToken, {
+        maxAge: cookieRTage,
         httpOnly: true,
-      }
-    )
-    delete data["accessToken"]
-  }
-  delete data["refreshToken"]
+    });
+    if (data.user?.role !== "admin") {
+        res.cookie("accessToken", data.accessToken, {
+            maxAge: cookieATage,
+            //httpOnly: true,
+        });
+        delete data["accessToken"];
+    }
+    delete data["refreshToken"];
 }
 
 export const authController = {
-  postLogin: async function (req, res, next) {
-    // passport.authenticate('local', function (error, user, info) {
-    //   if (error) {
-    //     return res.status(500).json(error);
-    //   }
-    //   if (!user) {
-    //     return res.status(404).json(info);
-    //   }
-    //   req.login(user, function (err) {
-    //     if (err) return next(err);
-    //     setCookie(res, user.user, 2 * 60 * 1000, 10 * 60 * 1000);
-    //     res.status(200).json(user);
-    //   })
-    // })(req, res, next);
-    const loginData = {
-      valueLogin: req.body.username,
-      password: req.body.password,
-      remember: req.body.remember
-    }
-    const result = await postLogin(loginData);
-    if (result.statusCode === 200) {
-      const rtExpiresTime = !loginData.remember ? cookieExpires.rfTokenNotRemember : cookieExpires.rfTokenRemember;
-      setCookie(res, result, cookieExpires.acToken, rtExpiresTime);
-    }
-    res.status(result.statusCode).json(result);
-  },
-  postRegister: async function (req, res, next) {
-    const origin = req.headers.origin; // hostname = 'localhost:5000'
-    const data = {
-      email: req.body.email,
-      username: req.body.username,
-      password: req.body.password,
-      serviceName: req.body.service,
-      serviceUrl: origin + "/",
-    }
-    const result = await registerUser(data);
-    return res.status(result.statusCode).json(result);
-  },
-  postForgotPassword: async function (req, res, next) {
-    const data = {
-      email: req.body?.email,
-      returnUrl: req.body?.returnUrl
-    }
-    if (!data?.email || !data.returnUrl) {
-      return res.status(400).json({
-        statusCode: 400,
-        message: "Invalid returnUrl or email."
-      });
-    }
-    const result = await forgotPassword(data);
-    res.status(result.statusCode).json(result);
-  },
-  postResetPassword: async function (req, res, next) {
-    const data = {
-      password: req.body?.password,
-      token: req.body?.token
-    }
-    const result = await resetPassword(data);
-    res.status(result.statusCode).json(result);
-  },
+    postLogin: async function (req, res, next) {
+        // passport.authenticate('local', function (error, user, info) {
+        //   if (error) {
+        //     return res.status(500).json(error);
+        //   }
+        //   if (!user) {
+        //     return res.status(404).json(info);
+        //   }
+        //   req.login(user, function (err) {
+        //     if (err) return next(err);
+        //     setCookie(res, user.user, 2 * 60 * 1000, 10 * 60 * 1000);
+        //     res.status(200).json(user);
+        //   })
+        // })(req, res, next);
+        const loginData = {
+            valueLogin: req.body.username,
+            password: req.body.password,
+            remember: req.body.remember,
+        };
+        const result = await postLogin(loginData);
+        if (result.statusCode === 200) {
+            const rtExpiresTime = !loginData.remember
+                ? cookieExpires.rfTokenNotRemember
+                : cookieExpires.rfTokenRemember;
+            setCookie(res, result, cookieExpires.acToken, rtExpiresTime);
+        }
+        res.status(result.statusCode).json(result);
+    },
+    postRegister: async function (req, res, next) {
+        const origin = req.headers.origin; // hostname = 'localhost:5000'
+        const data = {
+            email: req.body.email,
+            username: req.body.username,
+            password: req.body.password,
+            serviceName: req.body.service,
+            serviceUrl: origin + "/",
+        };
+        const result = await registerUser(data);
+        return res.status(result.statusCode).json(result);
+    },
+    postForgotPassword: async function (req, res, next) {
+        const data = {
+            email: req.body?.email,
+            returnUrl: req.body?.returnUrl,
+        };
+        if (!data?.email || !data.returnUrl) {
+            return res.status(400).json({
+                statusCode: 400,
+                message: "Invalid returnUrl or email.",
+            });
+        }
+        const result = await forgotPassword(data);
+        res.status(result.statusCode).json(result);
+    },
+    postResetPassword: async function (req, res, next) {
+        const data = {
+            password: req.body?.password,
+            token: req.body?.token,
+        };
+        const result = await resetPassword(data);
+        res.status(result.statusCode).json(result);
+    },
 
-  postChangePassword: async function (req, res, next) {
-    const data = {
-      currentPassword: req.body?.currentPassword,
-      newPassword: req.body?.newPassword,
-      userId: req.body?.userId
-    };
-    console.log(data)
-    const result = await changePassword(data);
-    res.status(result.statusCode).json(result);
-  },
+    postChangePassword: async function (req, res, next) {
+        const data = {
+            currentPassword: req.body?.currentPassword,
+            newPassword: req.body?.newPassword,
+            userId: req.body?.userId,
+        };
+        console.log(data);
+        const result = await changePassword(data);
+        res.status(result.statusCode).json(result);
+    },
 
-  getConfirmEmail: async function (req, res, next) {
-    const data = {
-      email: req.query?.email,
-      token: req.query?.token,
-      serviceName: req.query?.serviceName
-    }
-    if (!data.email || !data.token) {
-      return res.status(400).json({
-        statusCode: 400,
-        message: "Invalid email or token."
-      });
-    }
-    if (!data.serviceName) {
-      return res.status(400).json({
-        statusCode: 400,
-        message: "ServiceName not provice."
-      });
-    }
-    const result = await confirmEmail(data);
-    res.status(result.statusCode).json(result);
-  },
-  getEnableF2A: async function (req, res, next) {
-    const data = {
-      phone: req.query?.phone,
-    }
-    const result = await enableF2A(data);
-    res.status(result.statusCode).json(result);
-  },
-  postVerifyOtp: async function (req, res, next) {
-    const data = req.body;
-    const type = req.query?.type;
-    const result = await verifyOtp(data, type);
-    res.status(result.statusCode).json(result);
-  },
-  postLogout: async function (req, res, next) {
-    const rfToken = req.cookies?.refreshToken;
-    const acToken = req.cookies?.accessToken;
-    const userId = req.body?.userId;
-    const result = await postLogout(rfToken, acToken, userId);
-    res.clearCookie('accessToken');
-    res.clearCookie('refreshToken');
-    return res.status(result.statusCode).json(result);
-    // req.logout(function (err) {
-    //   if (err) { return next(err); }
-    //   req.session.destroy(function (err) {
-    //     if (!err) {
-    //       res.clearCookie('connect.sid', { path: '/' });
-    //       return res.status(200).json({
-    //         message: "Logout successfully.",
-    //         statusCode: 200
-    //       })
-    //     }
-    //     else {
-    //       console.log(err);
-    //       return res.status(400).json({
-    //         message: "Logout failed.",
-    //         statusCode: 400
-    //       })
-    //     }
-    //   })
-    // });
-  },
-  postGoogleLogin: async function (req, res, next) {
-    const data = req.body;
-    const result = await loginGoogle(data);
-    if (result.statusCode === 200) {
-      setCookie(res, result, cookieExpires.acToken, cookieExpires.rfTokenNotRemember);
-    }
-    res.status(result.statusCode).json(result);
-  },
-  postFacebookLogin: async function (req, res, next) {
-    const params = {
-      type: req.query.type,
-      userId: req.query?.userId,
-      facebookAccessToken: req.query.facebookAccessToken,
-      serviceName: req.query.serviceName
-    }
-    const data = await loginFacebook(params);
-    if (data.statusCode === 200 && params.type === "login") {
-      setCookie(res, data, cookieExpires.acToken, cookieExpires.rfTokenNotRemember);
-    }
-    res.status(data.statusCode).json(data);
-  },
-  postGoogleLink: async function (req, res, next) {
-    const userId = req.query.userId;
-    const data = req.body;
-    const result = await googleLink(userId, data);
-    res.status(result.statusCode).json(result);
-  },
+    getConfirmEmail: async function (req, res, next) {
+        const data = {
+            email: req.query?.email,
+            token: req.query?.token,
+            serviceName: req.query?.serviceName,
+        };
+        if (!data.email || !data.token) {
+            return res.status(400).json({
+                statusCode: 400,
+                message: "Invalid email or token.",
+            });
+        }
+        if (!data.serviceName) {
+            return res.status(400).json({
+                statusCode: 400,
+                message: "ServiceName not provice.",
+            });
+        }
+        const result = await confirmEmail(data);
+        res.status(result.statusCode).json(result);
+    },
+    getEnableF2A: async function (req, res, next) {
+        const data = {
+            phone: req.query?.phone,
+        };
+        const result = await enableF2A(data);
+        res.status(result.statusCode).json(result);
+    },
+    postVerifyOtp: async function (req, res, next) {
+        const data = req.body;
+        const type = req.query?.type;
+        const result = await verifyOtp(data, type);
+        res.status(result.statusCode).json(result);
+    },
+    postLogout: async function (req, res, next) {
+        const rfToken = req.cookies?.refreshToken;
+        const acToken = req.cookies?.accessToken;
+        const userId = req.body?.userId;
+        const result = await postLogout(rfToken, acToken, userId);
+        res.clearCookie("accessToken");
+        res.clearCookie("refreshToken");
+        return res.status(result.statusCode).json(result);
+        // req.logout(function (err) {
+        //   if (err) { return next(err); }
+        //   req.session.destroy(function (err) {
+        //     if (!err) {
+        //       res.clearCookie('connect.sid', { path: '/' });
+        //       return res.status(200).json({
+        //         message: "Logout successfully.",
+        //         statusCode: 200
+        //       })
+        //     }
+        //     else {
+        //       console.log(err);
+        //       return res.status(400).json({
+        //         message: "Logout failed.",
+        //         statusCode: 400
+        //       })
+        //     }
+        //   })
+        // });
+    },
+    postGoogleLogin: async function (req, res, next) {
+        const data = req.body;
+        const result = await loginGoogle(data);
+        if (result.statusCode === 200) {
+            setCookie(
+                res,
+                result,
+                cookieExpires.acToken,
+                cookieExpires.rfTokenNotRemember
+            );
+        }
+        res.status(result.statusCode).json(result);
+    },
+    postFacebookLogin: async function (req, res, next) {
+        const params = {
+            type: req.query.type,
+            userId: req.query?.userId,
+            facebookAccessToken: req.query.facebookAccessToken,
+            serviceName: req.query.serviceName,
+        };
+        const data = await loginFacebook(params);
+        if (data.statusCode === 200 && params.type === "login") {
+            setCookie(
+                res,
+                data,
+                cookieExpires.acToken,
+                cookieExpires.rfTokenNotRemember
+            );
+        }
+        res.status(data.statusCode).json(data);
+    },
+    postGoogleLink: async function (req, res, next) {
+        const userId = req.query.userId;
+        const data = req.body;
+        const result = await googleLink(userId, data);
+        res.status(result.statusCode).json(result);
+    },
 
-  deleteUnlinkProvider: async function (req, res, next) {
-    const data = {
-      userId: req.query?.userId,
-      providerId: req.query?.providerId
-    }
-    const result = await unlinkGoogle(data);
-    res.status(result.statusCode).json(result);
-  },
-  postVerifyToken: async function (req, res, next) {
-    let accessToken = req.header('authorization')?.split(' ')[1];
-    if (!accessToken) accessToken = req.cookies?.accessToken;
-    const result = verifyJWT(accessToken);
-    res.status(result.statusCode).json(result);
-  },
-  postVerifyFacebookToken: async function (req, res, next) {
-    let fbAccessToken = req.header('authorization')?.split(' ')[1];
-    if (!fbAccessToken) fbAccessToken = req.cookies?.accessToken;
-    const result = verifyFacebookJWT(fbAccessToken);
-    res.status(result.statusCode).json(result);
-  },
-  postVerifyGoogleToken: async function (req, res, next) {
-    let ggAccessToken = req.header('authorization')?.split(' ')[1];
-    if (!ggAccessToken) ggAccessToken = req.cookies?.accessToken;
-    const result = await verifyGoogleJWT(ggAccessToken);
-    res.status(result?.statusCode).json(result);
-  },
-  getRefreshToken: async function (req, res, next) {
-    const rfToken = req.cookies?.refreshToken;
-    const remember = req.query?.remember;
-    const type = req.query?.type;
-    const result = await refreshToken(rfToken, type, remember);
-    if (result.statusCode === 200) {
-      let rtExpiresTime;
-      if (type === 'default') {
-        rtExpiresTime = remember === "false" ? cookieExpires.rfTokenNotRemember : cookieExpires.rfTokenRemember;
-      } else {
-        rtExpiresTime = cookieExpires.rfTokenNotRemember;
-      }
-      setCookie(res, result, cookieExpires.acToken, rtExpiresTime);
-    }
-    delete result['user']
-    res.status(result.statusCode).json(result);
-  },
-}
+    deleteUnlinkProvider: async function (req, res, next) {
+        const data = {
+            userId: req.query?.userId,
+            providerId: req.query?.providerId,
+        };
+        const result = await unlinkGoogle(data);
+        res.status(result.statusCode).json(result);
+    },
+    postVerifyToken: async function (req, res, next) {
+        let accessToken = req.header("authorization")?.split(" ")[1];
+        if (!accessToken) accessToken = req.cookies?.accessToken;
+        const result = verifyJWT(accessToken);
+        res.status(result.statusCode).json(result);
+    },
+    postVerifyFacebookToken: async function (req, res, next) {
+        let fbAccessToken = req.header("authorization")?.split(" ")[1];
+        if (!fbAccessToken) fbAccessToken = req.cookies?.accessToken;
+        const result = verifyFacebookJWT(fbAccessToken);
+        res.status(result.statusCode).json(result);
+    },
+    postVerifyGoogleToken: async function (req, res, next) {
+        let ggAccessToken = req.header("authorization")?.split(" ")[1];
+        if (!ggAccessToken) ggAccessToken = req.cookies?.accessToken;
+        const result = await verifyGoogleJWT(ggAccessToken);
+        res.status(result?.statusCode).json(result);
+    },
+    getRefreshToken: async function (req, res, next) {
+        const rfToken = req.cookies?.refreshToken;
+        const remember = req.query?.remember;
+        const type = req.query?.type;
+        const result = await refreshToken(rfToken, type, remember);
+        if (result.statusCode === 200) {
+            let rtExpiresTime;
+            if (type === "default") {
+                rtExpiresTime =
+                    remember === "false"
+                        ? cookieExpires.rfTokenNotRemember
+                        : cookieExpires.rfTokenRemember;
+            } else {
+                rtExpiresTime = cookieExpires.rfTokenNotRemember;
+            }
+            setCookie(res, result, cookieExpires.acToken, rtExpiresTime);
+        }
+        delete result["user"];
+        res.status(result.statusCode).json(result);
+    },
+};
--- src/services/jwtService.js
+++ src/services/jwtService.js
@@ -1,85 +1,95 @@
 import jwt from "jsonwebtoken";
-const uuid = require('uuid');
-const fs = require('fs');
-const { OAuth2Client } = require('google-auth-library');
+const uuid = require("uuid");
+const fs = require("fs");
+const { OAuth2Client } = require("google-auth-library");
 
 const createJWT = (payload) => {
-  return jwt.sign(payload, process.env.SECRET, {
-    algorithm: 'HS256',
-    expiresIn: +process.env.EXPIRES,
-    issuer: process.env.ISSUER,
-    audience: process.env.AUDIENCE,
-  })
-}
+    return jwt.sign(payload, process.env.SECRET, {
+        algorithm: "HS256",
+        expiresIn: +process.env.EXPIRES,
+        issuer: process.env.ISSUER,
+        audience: process.env.AUDIENCE,
+    });
+};
 
 const createFacebookJWT = (payload) => {
-  return jwt.sign(payload, process.env.FB_APP_SECRET, {
-    algorithm: 'HS256',
-    expiresIn: +process.env.EXPIRES,
-    issuer: process.env.FB_ISSUER,
-    audience: process.env.FB_APP_ID,
-  })
-}
+    return jwt.sign(payload, process.env.FB_APP_SECRET, {
+        algorithm: "HS256",
+        expiresIn: +process.env.EXPIRES,
+        issuer: process.env.FB_ISSUER,
+        audience: process.env.FB_APP_ID,
+    });
+};
 
 const createRefreshToken = () => {
-  return uuid.v4();
-}
+    return uuid.v4();
+};
 
 const verifyJWT = (token) => {
-  const secret = process.env.SECRET;
-  try {
-    jwt.verify(token, secret, {
-      issuer: process.env.ISSUER,
-      audience: process.env.AUDIENCE,
-    });
-    return { statusCode: 200, message: "Verify successfully.", token };
-  } catch (error) {
-    return {
-      statusCode: 401,
-      message: error.message
-    };
-  }
-}
+    const secret = process.env.SECRET;
+    try {
+        jwt.verify(token, secret, {
+            issuer: process.env.ISSUER,
+            audience: process.env.AUDIENCE,
+        });
+        return { statusCode: 200, message: "Verify successfully.", token };
+    } catch (error) {
+        return {
+            statusCode: 401,
+            message: error.message,
+        };
+    }
+};
 
 const verifyFacebookJWT = (fbToken) => {
-  const secret = process.env.FB_APP_SECRET;
-  try {
-    jwt.verify(fbToken, secret, {
-      audience: process.env.FB_APP_ID,
-      issuer: process.env.FB_ISSUER,
-    });
-    return { statusCode: 200, message: "Verify successfully.", token: fbToken };
-  } catch (error) {
-    return {
-      statusCode: 401,
-      message: error.message
-    };
-  }
-}
+    const secret = process.env.FB_APP_SECRET;
+    try {
+        jwt.verify(fbToken, secret, {
+            audience: process.env.FB_APP_ID,
+            issuer: process.env.FB_ISSUER,
+        });
+        return {
+            statusCode: 200,
+            message: "Verify successfully.",
+            token: fbToken,
+        };
+    } catch (error) {
+        return {
+            statusCode: 401,
+            message: error.message,
+        };
+    }
+};
 
 const verifyGoogleJWT = async (ggToken) => {
-  try {
-    const certs = JSON.parse(fs.readFileSync('./././google-cerfiticates.json'))
-    const client = new OAuth2Client();
-    await client.verifySignedJwtWithCertsAsync(ggToken, certs)
-    // await client.verifyIdToken({
-    //   idToken: ggToken,
-    //   audience: process.env.CLIENT_ID,
-    // });
-    return { statusCode: 200, message: "Verify successfully google.", token: ggToken };
-  } catch (error) {
-    return {
-      statusCode: 401,
-      message: error.message
-    };
-  }
-}
+    try {
+        const certs = JSON.parse(
+            fs.readFileSync("./././google-cerfiticates.json")
+        );
+        const client = new OAuth2Client();
+        await client.verifySignedJwtWithCertsAsync(ggToken, certs);
+        // await client.verifyIdToken({
+        //   idToken: ggToken,
+        //   audience: process.env.CLIENT_ID,
+        // });
+        return {
+            statusCode: 200,
+            message: "Verify successfully google.",
+            token: ggToken,
+        };
+    } catch (error) {
+        return {
+            statusCode: 401,
+            message: error.message,
+        };
+    }
+};
 
 export {
-  createJWT,
-  verifyJWT,
-  verifyGoogleJWT,
-  verifyFacebookJWT,
-  createFacebookJWT,
-  createRefreshToken
-}
+    createJWT,
+    verifyJWT,
+    verifyGoogleJWT,
+    verifyFacebookJWT,
+    createFacebookJWT,
+    createRefreshToken,
+};
